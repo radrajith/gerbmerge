b27afc093c9dc0b84af7e513831e9b2e67615c5d
Rajith Radhakrishnan <radrajith@gmail.com>
2016-10-28 16:26:44 -0400

    added files

---
+++ drillfix.py
@@ -0,0 +1,31 @@
+#enter file name
+file = open('merge2.TXT', 'r+') #open merge2.txt file as a read write option
+output = open('merge2_corrected.TXT','a')
+list = []
+i=0
+for line in file:
+    if 'T' in line:
+        list.append(line)
+        #print line
+        #newLine = line[:3]
+        #output.write(newLine + '\n')
+    #else:
+        #output.write(line)
+output.seek(0)
+output.write('%\nM48\nM74\n')
+for i in list:
+    output.write(i)
+output.write('%\n')
+file.seek(0)
+for line in file:
+    if '%' in line:
+        for line in file:
+            if 'T' in line:
+                #list.append(line)
+                #print line
+                newLine = line[:3]
+                output.write(newLine + '\n')
+            else:
+                output.write(line)
+file.close()
+output.close()
